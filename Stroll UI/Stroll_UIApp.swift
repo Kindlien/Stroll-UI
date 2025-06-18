2d345c50a1c85bc35ef1e5504890390a360ce800
William Kindlien <william.k.gunawan@gmail.com>
2025-06-18 22:27:04 +0700

    Initial Commit

---
+++ Stroll UI/Stroll_UIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Stroll_UIApp.swift
+//  Stroll UI
+//
+//  Created by William Kindlien Gunawan on 18/06/25.
+//
+
+import SwiftUI
+
+@main
+struct Stroll_UIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
